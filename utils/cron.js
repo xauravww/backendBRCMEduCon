442767782fbdc796ca9e781ed96b633675e8d2ea
xauravww <sauravmaheshwari8@gmail.com>
2024-04-20 02:20:14 +0530

    updated cron js imports

--- utils/cron.js
+++ utils/cron.js
@@ -1,32 +1,33 @@
-import { createRequire } from "module"
-const require = createRequire(import.meta.url)
+const cron = require("cron");
+const https = require("https");
+const path = require("path");
+const dotenv = require("dotenv");
 
-import cron from "cron"
-const https = require("https")
-const path = require("path")
-const dotenv = require("dotenv")
-dotenv.config({ path: path.resolve(".env") })
-const backendUrl = process.env.RENDER_BACKEND_URL
 
+// Load environment variables from .env file
+dotenv.config({ path: path.resolve(".env") });
+const backendUrl = process.env.RENDER_BACKEND_URL;
+
+// Define the cron job
 const job = new cron.CronJob("0 */13 * * * *", function () {
   // This function will be executed every 14 minutes.
-  // console.log("Restarting server")
+  // console.log("Restarting server");
 
   // Perform an HTTPS GET request to hit any backend api.
   https
     .get(backendUrl, (res) => {
       if (res.statusCode === 200) {
-        // console.log("Server restarted")
+        // console.log("Server restarted");
       } else {
         console.error(
           `Failed to restart server with status code: ${res.statusCode}`
-        )
+        );
       }
     })
     .on("error", (err) => {
-      console.error("Error during Restart:", err.message)
-    })
-})
+      console.error("Error during Restart:", err.message);
+    });
+});
 
 // Export the cron job.
-export { job }
+module.exports = { job };
