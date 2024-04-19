a5417d531685acafdcea5817f4a178fd2af9d1db
xauravww <sauravmaheshwari8@gmail.com>
2024-04-20 02:25:01 +0530

    removed .js in job import

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require("./app");
 const cloudinary = require("cloudinary");
 const connectDatabase = require("./config");
-const { job } = require("./utils/cron.js");
+const { job } = require("./utils/cron");
 job.start()
 
 // Handling Uncaught Exception
