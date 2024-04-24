24202148d53c06eae2d29b7c0a89ef21cf09a723
xauravww <sauravmaheshwari8@gmail.com>
2024-04-24 21:14:20 +0530

    Added: student api monthly attendance

--- routes/attendanceRoute.js
+++ routes/attendanceRoute.js
@@ -6,6 +6,7 @@ const {
   getAllAttendance,
   getStudentsForAttendance,
   getUniqueAttendance,
+  getMonthlyStudentAttendance
 } = require("../controllers/attendanceController");
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
@@ -19,9 +20,13 @@ router.route("/faculty/attendance/unique").post(
   // isAuthenticatedUser, authorizeRoles("admin","faculty"),
   getUniqueAttendance);
 
+// get monthly attendance using branch, sem , subject and  roll number
+router.route("/student/attendance").post(getMonthlyStudentAttendance)
+
+
 // to get students For Attendance of a paticular Branch and Semester
 
-router.route("/faculty/attendance/students").post(getStudentsForAttendance);
+router.route("/faculty/attendance/students").get(getStudentsForAttendance);
 
 router
   .route("/faculty/attendance")
